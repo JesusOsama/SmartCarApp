e8b5df0783056b3173b5f5891d981a2927f676d7
JesusOsama <jesusv2911@gmail.com>
2022-10-15 21:13:57 -0500

    Navegación entre fragments 1.1

---
+++ app/src/main/java/com/example/smartcarapp/ui/fragments/Compras/compras_fecha.kt
@@ -0,0 +1,20 @@
+package com.example.smartcarapp.ui.fragments.Compras
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartcarapp.R
+
+class compras_fecha : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout._compras_fecha, container, false)
+    }
+
+}
