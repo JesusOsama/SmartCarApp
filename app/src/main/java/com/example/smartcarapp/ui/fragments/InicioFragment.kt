de6930a6ab975b571e38709c1d0b601e60a728c4
JesusOsama <jesusv2911@gmail.com>
2022-10-15 21:12:13 -0500

    Navegación entre fragments

--- app/src/main/java/com/example/smartcarapp/ui/fragments/InicioFragment.kt
+++ app/src/main/java/com/example/smartcarapp/ui/fragments/InicioFragment.kt
@@ -1,10 +1,18 @@
 package com.example.smartcarapp.ui.fragments
+
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.smartcarapp.R
+import com.example.smartcarapp.ui.Cliente_Activity
+import com.example.smartcarapp.ui.fragments.Compras.compras_fecha
 
 
 class InicioFragment : Fragment() {
@@ -14,8 +22,15 @@ class InicioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
-    }
+        val view: View = inflater.inflate(R.layout.fragment_inicio, container, false)
 
+        val btnBusqueda: Button = view.findViewById(R.id.btn_busqueda)
+
+        btnBusqueda.setOnClickListener{ view ->
+            view.findNavController().navigate(R.id.nav_inicio_compras_fecha)
+        }
+
+        return view
+    }
 
 }
